f1e72c27a6a912306f841d8602862c6b4de0bf63
Bhagwati Pandey <shonena4u@gmail.com>
2017-04-19 13:40:44 +0530

    final

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 // Code goes here
 
-$(document).ready(function(){
-  $(document).on('click', '.tripType .btn', function(){
-    var _ = $(this);
-    if(_.hasClass('active')){
-     // _.removeClass('active');
-    }
-    else{
-      _.siblings().removeClass('active');
-      _.addClass('active');
-    }
-    //console.log(_);
-  });
+$(document).ready(function () {
+    $(document).on('click', '.tripType .btn', function () {
+        var _ = $(this);
+        if (_.hasClass('active')) {
+            // _.removeClass('active');
+        }
+        else {
+            _.siblings().removeClass('active');
+            _.addClass('active');
+        }
+        //console.log(_);
+    });
+    $(document).on('click', '#source', function () {
+        var _ = $(this);
+        $('.secondary').slideDown('slow');
+        $('.closeIt').slideDown('slow');
+        //console.log(_);
+    });
+    $(document).on('click', '.closeIt', function () {
+        var _ = $(this);
+        $('.secondary').slideUp('slow');
+        $('.closeIt').slideUp('slow');
+        //console.log(_);
+    });
 });
